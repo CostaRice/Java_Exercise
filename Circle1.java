cbcc6732ac4790893b40d8c8b2d8f8b2b3f40584
CostaHu <hyc2723@gmail.com>
2016-03-26 00:21:11 +0800

    Add a way of combining two classes

---
+++ Circle1.java
@@ -0,0 +1,24 @@
+/**
+ * Created by CostaHu on 2016/3/25.
+ */
+public class Circle1 {
+    public static void main(String[] args) {
+
+        Circle1 circle1 = new Circle1();
+        System.out.println("The area of circle of radius " + circle1.radius +
+                " is " +circle1.getArea() );
+
+        Circle1 circle12 = new Circle1();
+    }
+    double radius;
+    Circle1(){
+        radius = 1.0;
+    }
+    Circle1(double newRadius){
+        radius = newRadius;
+    }
+    double getArea(){
+        return radius*radius*Math.PI;
+    }
+
+}
