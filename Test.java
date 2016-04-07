65bafe223e4d18073b7b0543769cb5e346bea8be
CostaHu <hyc2723@gmail.com>
2016-04-07 17:04:30 +0800

    Add an instance for usage of this

---
+++ Test.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by CostaHu on 2016/4/6.
+ */
+public class Test {
+     static class Variable{
+        int x,y,z=0;
+        void init(int x,int y){
+            this.x = x;
+            this.y = y;
+            int z = 5;
+            System.out.println("*****IN INIT*****");
+            System.out.println("x = "+x+ " y = "+y+" z = "+z);
+        }
+    }
+    public static void main(String[] args){
+        Variable variable = new Variable();
+        System.out.println("*****BEFORE INIT*****");
+        System.out.println(variable.x+variable.y+variable.z);
+        variable.init(10,30);
+        System.out.println("*****AFTER INIT*****");
+        System.out.println(variable.x+variable.y+variable.z);
+    }
+}
