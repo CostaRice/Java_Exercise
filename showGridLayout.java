ab338ff2754ca2bc53b4b799b57282973af1cbd6
CostaHu <hyc2723@gmail.com>
2016-04-10 00:00:14 +0800

    Update showGridLayout.java

--- showGridLayout.java
+++ showGridLayout.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by CostaHu on 2016/4/9.
  */
-public class showGridLayout {
+public class ShowGridLayout extends JFrame{
+    public ShowGridLayout(){
+        setLayout(new GridLayout(3,2,8,5));
+        add(new JLabel("First Name"));
+        add(new JTextField(8));
+        add(new JLabel("MI"));
+        add(new JTextField(1));
+        add(new JLabel("Last Name"));
+        add(new JTextField(8));
+    }
+
+    public static void main(String[] args) {
+        ShowGridLayout frame = new ShowGridLayout();
+        frame.setTitle("ShowGridLayout");
+        frame.setSize(200,125);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
